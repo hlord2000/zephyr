080b7fe677fc05aa9b02502a4fe704c229d9a301
Kelly Helmut Lord <kellyhlord@gmail.com>
2025-08-24 00:23:43 -0400

    Added dts files, initial read logic for variants

--- drivers/sensor/sensirion/sen6x/sen6x.c
+++ drivers/sensor/sensirion/sen6x/sen6x.c
@@ -49,103 +49,93 @@ static int sen6x_write_command(const struct device *dev, uint16_t cmd_id, uint16
 	return i2c_write_dt(&cfg->bus, tx_buf, sizeof(tx_buf));
 }
 
-static int sen6x_read_sample(const struct device *dev, uint16_t cmd_id, uint16_t *sample)
+static int sen6x_read_command(const struct device *dev, uint16_t cmd_id, k_timeout_t execution_time, uint16_t *readings, size_t num_readings)
 {
 	const struct sen6x_config *cfg = dev->config;
 	uint8_t tx_buf[2] = {(cmd_id & 0xFF), ((cmd_id >> 8) & 0xFF)};
-	uint8_t rx_buf[3];
-	int rc;
+	uint8_t rx_buf[SEN6X_MAX_REGISTER_LEN];
+	int ret;
 
-	rc = i2c_write_read_dt(&cfg->bus, tx_buf, sizeof(tx_buf), rx_buf, sizeof(rx_buf));
-	if (rc < 0) {
-		LOG_ERR("Failed to read sensor.");
-		return rc;
+	ret = i2c_write_dt(&cfg->bus, tx_buf, sizeof(tx_buf));
+	if (ret < 0) {
+		LOG_ERR("Failed to initiate read command.");
+		return ret;
 	}
 
-	*sample = sys_get_be16(rx_buf);
-	if (sen6x_compute_crc(*sample) != rx_buf[2]) {
-		LOG_ERR("Invalid CRC for sensor reading.");
-		return -EIO;
+	k_sleep(execution_time);
+
+	ret = i2c_read_dt(&cfg->bus, rx_buf, num_readings * SEN6X_READ_WIDTH_BYTES);
+	if (ret < 0) {
+		LOG_ERR("Failed to read sensor.");
+		return ret;
+	}
+
+	uint16_t *reg_val;
+	for (int i = 0; i < num_readings; i++) {
+		reg_val = &rx_buf[i * SEN6X_READ_WIDTH_BYTES];
+		readings[i] = sys_get_be16(reg_val);
+		if (sen6x_compute_crc(reg_val) != rx_buf[(i * SEN6X_READ_WIDTH_BYTES) + 2]) {
+			LOG_ERR("Invalid CRC for read command.");
+			return -EIO;
+		}
 	}
 
 	return 0;
+}
+
+static int sen6x_read_sample_particulate_matter(const struct device *dev)
+{
+	const struct sen6x_config *cfg = dev->config;
+	struct sen6x_data *data = dev->data;
+	int ret;
+	uint16_t readings[SEN6X_MAX_READINGS_LEN];
+
 }
 
 static int sen6x_sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
 	const struct sen6x_config *cfg = dev->config;
 	struct sen6x_data *data = dev->data;
-	int rc;
+	int ret;
+
+	uint16_t readings[SEN6X_MAX_READINGS_LEN];
 
 	switch (cfg->variant) {
 	case DEVICE_VARIANT_SEN60:
+		ret = sen6x_read_command(dev, SEN60_CMD_READ_MEASURED_VALS, SEN60_READ_MEASURED_VALS_EXEC_TIME, readings, SEN60_NUM_MEASURED_VALS);
+		if (ret < 0) {
+			return ret;
+		}
+
 	case DEVICE_VARIANT_SEN63C:
 	case DEVICE_VARIANT_SEN65:
 	case DEVICE_VARIANT_SEN66:
 	case DEVICE_VARIANT_SEN68:
 	default:
-	}
-
-	if (chan != SENSOR_CHAN_ALL && chan != SENSOR_CHAN_AMBIENT_TEMP &&
-	    chan != SENSOR_CHAN_HUMIDITY) {
-		return -ENOTSUP;
-	}
-
-	rc = sen6x_write_command(dev, measure_cmd[cfg->repeatability]);
-	if (rc < 0) {
-		LOG_ERR("Failed to start measurement.");
-		return rc;
-	}
-
-	k_sleep(K_USEC(measure_wait_us[cfg->repeatability]));
-
-	rc = sen6x_read_sample(dev, &data->t_sample, &data->rh_sample);
-	if (rc < 0) {
-		LOG_ERR("Failed to fetch data.");
-		return rc;
+		return -EINVAL;
 	}
 
 	return 0;
 }
 
-static int sht4x_channel_get(const struct device *dev, enum sensor_channel chan,
+static int sen6x_channel_get(const struct device *dev, enum sensor_channel chan,
 			     struct sensor_value *val)
 {
-	const struct sht4x_data *data = dev->data;
-
-	/*
-	 * See datasheet "Conversion of Signal Output" section
-	 * for more details on processing sample data.
-	 */
-	if (chan == SENSOR_CHAN_AMBIENT_TEMP) {
-		int64_t tmp;
-
-		tmp = data->t_sample * 175;
-		val->val1 = (int32_t)(tmp / 0xFFFF) - 45;
-		val->val2 = ((tmp % 0xFFFF) * 1000000) / 0xFFFF;
-	} else if (chan == SENSOR_CHAN_HUMIDITY) {
-		uint64_t tmp;
-
-		tmp = data->rh_sample * 125U;
-		val->val1 = (uint32_t)(tmp / 0xFFFF) - 6U;
-		val->val2 = (tmp % 0xFFFF) * 15625U / 1024U;
-	} else {
-		return -ENOTSUP;
-	}
+	const struct sen6x_data *data = dev->data;
 
 	return 0;
 }
 
-static int sht4x_attr_set(const struct device *dev, enum sensor_channel chan,
+static int sen6x_attr_set(const struct device *dev, enum sensor_channel chan,
 			  enum sensor_attribute attr, const struct sensor_value *val)
 {
-	struct sht4x_data *data = dev->data;
+	struct sen6x_data *data = dev->data;
 
 	if (val->val1 < 0) {
 		return -EINVAL;
 	}
 
-	switch ((enum sensor_attribute_sht4x)attr) {
+	switch ((enum sensor_attribute_sen6x)attr) {
 	case SENSOR_ATTR_SHT4X_HEATER_POWER:
 		if (val->val1 > SHT4X_HEATER_POWER_IDX_MAX) {
 			return -EINVAL;
@@ -165,30 +155,30 @@ static int sht4x_attr_set(const struct device *dev, enum sensor_channel chan,
 	return 0;
 }
 
-static int sht4x_init_chip(const struct device *dev)
+static int sen6x_init_chip(const struct device *dev)
 {
-	int rc;
+	int ret;
 
 	/* 1 ms (max) power up time according to datasheet */
 	k_sleep(K_MSEC(SEN6X_POR_WAIT_MS));
 
-	rc = sht4x_write_command(dev, SHT4X_CMD_RESET);
-	if (rc < 0) {
+	ret = sen6x_write_command(dev, SHT4X_CMD_RESET);
+	if (ret < 0) {
 		LOG_ERR("Failed to reset the device.");
-		return rc;
+		return ret;
 	}
 
 	k_sleep(K_MSEC(SHT4X_RESET_WAIT_MS));
 	return 0;
 }
 
-static int sht4x_pm_action(const struct device *dev, enum pm_device_action action)
+static int sen6x_pm_action(const struct device *dev, enum pm_device_action action)
 {
-	int rc = 0;
+	int ret = 0;
 
 	switch (action) {
 	case PM_DEVICE_ACTION_TURN_ON:
-		rc = sht4x_init_chip(dev);
+		ret = sen6x_init_chip(dev);
 		break;
 
 	case PM_DEVICE_ACTION_RESUME:
@@ -200,7 +190,7 @@ static int sht4x_pm_action(const struct device *dev, enum pm_device_action actio
 		return -ENOTSUP;
 	}
 
-	return rc;
+	return ret;
 }
 
 static int sen6x_init(const struct device *dev)
@@ -215,13 +205,6 @@ static int sen6x_init(const struct device *dev)
 	return pm_device_driver_init(dev, sen6x_pm_action);
 }
 
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
->>>>>>> 3e8e26dea49 (Finished registers, beginning implementation.)
-=======
->>>>>>> 3e8e26dea49 (Finished registers, beginning implementation.)
 static DEVICE_API(sensor, sen6x_api) = {
 	.sample_fetch = sen6x_sample_fetch,
 	.channel_get = sen6x_channel_get,
@@ -229,8 +212,6 @@ static DEVICE_API(sensor, sen6x_api) = {
 	.attr_get = sen6x_attr_set,
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 #define SEN6X_DEFINE(n, compat, variant_enum)                                                      \
 	static struct sen6x_data sen6x_data_##n;                                                   \
                                                                                                    \
@@ -240,9 +221,7 @@ static DEVICE_API(sensor, sen6x_api) = {
 	SENSOR_DEVICE_DT_INST_DEFINE(n, sen6x_init, PM_DEVICE_DT_INST_GET(n), &sen6x_data_##n,     \
 				     &sen6x_config_##n, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,  \
 				     &sen6x_api);
-=======
-=======
->>>>>>> 3e8e26dea49 (Finished registers, beginning implementation.)
+
 #define SEN6X_DEFINE(n, compat, variant_enum)				\
 	static struct sen6x_data sen6x_data_##n;		\
 								\
@@ -259,10 +238,6 @@ static DEVICE_API(sensor, sen6x_api) = {
 				POST_KERNEL,			\
 				CONFIG_SENSOR_INIT_PRIORITY,	\
 				&sen6x_api);
-<<<<<<< HEAD
->>>>>>> 3e8e26dea49 (Finished registers, beginning implementation.)
-=======
->>>>>>> 3e8e26dea49 (Finished registers, beginning implementation.)
 
 #define DT_DRV_COMPAT sensirion_sen60
 DT_INST_FOREACH_STATUS_OKAY_VARGS(SEN6X_DEFINE, DT_DRV_COMPAT, DEVICE_VARIANT_SEN60)
